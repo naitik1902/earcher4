4580e76ff3de28521227c9794d7a5021d8b11a76
naitik1902 <139570941+naitik1902@users.noreply.github.com>
2023-09-29 01:13:00 +0530

    Add files via upload

---
+++ project-template-C31-main/project-template-C31-main/js/PlayerArcher.js
@@ -0,0 +1,53 @@
+class PlayerArcher {
+    constructor(x, y, width, height) {
+      const options = {
+        isStatic: true
+      };
+  
+      this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      //this.collapse = false;
+      this.image = loadImage
+      ("./assets/playerArcher.png");
+      this.trajectory = []
+      World.add(world, this.body);
+  
+      Matter.Body.setAngle(this.body, -PI / 2); 
+      //Matter.Body.setAngle(this.body, -PI ); 
+      //Matter.Body(this.body, -PI ); 
+      //Matter.setAngle(this.body, -PI / 2); 
+    }
+  
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      
+      if (keyIsDown(DOWN_ARROW) && angle < -1.2) {
+        angle += 0.01;
+        Matter.Body.setAngle(this.body, angle);
+      }
+  
+      if (keyIsDown(UP_ARROW) && angle > -1.9) {
+        angle -= 0.01;
+        Matter.Body.setAngle(this.body, angle);
+      }
+
+       if (this.body.velocity.x > 0 && this.body.position.x > 200) {
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+
+    for (var i = 0; i < this.trajectory.length; i++) {
+      image(this.image, this.trajectory[i][0], this.trajectory[i][1], 6, 6);
+    }
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
+  
